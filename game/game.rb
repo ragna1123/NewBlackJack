89c4887b9110e196afcc5063873d843a9cbecd05
haga_hiroki <ragna.1123@gmail.com>
2023-11-02 23:42:09 +0900

    メッセージの分岐処理

--- game/game.rb
+++ game/game.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require './player/player'
 class Game
   def initialize
--- player/player.rb
+++ player/player.rb
@@ -11,29 +11,60 @@ class User
 
   def drow
     # 一番上から取得して@handへ渡す破壊的処理
-    deck = @deck.deck
-    @hand << deck.first
-    deck.delete(deck[0])
+      deck = @deck.deck
+      @hand << deck.first
+      deck.delete(deck[0])
   end
+
   # デッキからカードを引いて配列に入れる
   def drow_message
-    puts "#{@name}は#{@hand}を引きました"
+    if self.class == Player || @hand.length <= 1 || @hand.length >= 3
+      puts "#{@name}は#{@hand.last[0]}の#{@hand.last[1]}を引きました"
+    elsif self.class == Dealer || Cpu
+      puts "#{@name}の引いた２枚目のカードは分かりません"
+    end
+  end
+
+  # 手持ち札の計算をする
+  def sum_hand_pint
+    point_array = @hand.map{|card|card[1]} 
+    # ここに配列の合計値を正しJQKは10ポイントにする
   end
 end
 
 class Player < User
-  # chipの持ち点とベット
-  # ユーザーの手持ちが２枚の時に引く処理
-  # メッセージ
+  def player_next_hand?
+    puts "あなたの現在の得点は#{}です。カードを引きますか？（Y/N）"
+    next_hand = gets.chomp
+    if %w[Y y].include?(next_hand)
+      true
+    elsif %w[N n].include?(next_hand)
+      false
+    else
+      player_next_hand?
+    end
+  end
 end
 
 class Cpu < User
-  def cpu_move
+  # CPUの挙動
+  def cpu_next_hand?
+    if @own_point <= 16
+      true
+    elsif @own_point >= 17
+      false
+    end
   end
-  # CPUのメッセージ処理
 end
 
-Player.new("haga")
+# player = Player.new("haga")
+# player.drow
+# player.drow
+# player.drow
+# player.drow
+# player.drow
+
+# p player.sum_hand_pint
 
 class Dealer < Cpu
 
